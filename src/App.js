5d2234c361cad87a018ce1f8f53ec35f55c2788c
SinghDigamber <digambersingh126@gmail.com>
2019-09-17 01:16:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import FormDataComponent from './components/form-data.component';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <FormDataComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/form-data.component.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+
+export default class FormDataComponent extends Component {
+    userData;
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePhone = this.onChangePhone.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            name: '',
+            email: '',
+            phone: ''
+        }
+    }
+
+    // Form Values
+    onChangeName(e) {
+        this.setState({ name: e.target.value })
+    }
+
+    onChangeEmail(e) {
+        this.setState({ email: e.target.value })
+    }
+
+    onChangePhone(e) {
+        this.setState({ phone: e.target.value })
+    }
+
+
+    // React Life Cycle
+    componentDidMount() {
+        this.userData = JSON.parse(localStorage.getItem('user'));
+
+        if (localStorage.getItem('user')) {
+            this.setState({
+                name: this.userData.name,
+                email: this.userData.email,
+                phone: this.userData.phone
+            })
+        } else {
+            this.setState({
+                name: '',
+                email: '',
+                phone: ''
+            })
+        }
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        localStorage.setItem('user', JSON.stringify(nextState));
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+        console.log(this.state.props)
+    }
+
+
+    render() {
+        return (
+            <div className="container">
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input type="text" className="form-control" value={this.state.name} onChange={this.onChangeName} />
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input type="email" className="form-control" value={this.state.email} onChange={this.onChangeEmail} />
+                    </div>
+                    <div className="form-group">
+                        <label>Phone</label>
+                        <input type="tel" className="form-control" value={this.state.phone} onChange={this.onChangePhone} />
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
